eb975cd775bfaf1eddb8641f9e70217271830935
Alex <apjames93@gmail.com>
2017-08-09 15:25:55 -0600

    Fixing templates

---
+++ values.js
@@ -0,0 +1,48 @@
+$scope.values = {
+  "customer": {
+    "name": "Something cool INC"
+  },
+  "contact": {
+    "name": "Alex James",
+    "email": "apjames93@gmail.com",
+    "workPhone": "3037209905"
+  },
+  "serviceAccountMeter": [{
+      service_address: "260 lol at you ln somewhere NA 85236",
+      account_number: 90909090,
+      meter_number: 3030303,
+    },
+    {
+      service_address: "260 lol at you ln somewhere NA 85236",
+      account_number: 90909090,
+      meter_number: 3030303,
+    },
+    {
+      service_address: "260 lol at you ln somewhere NA 85236",
+      account_number: 90909090,
+      meter_number: 3030303,
+    },
+  ],
+  term : {
+    program :  'Percent Savings',
+    // program :  'Fixed Price',
+    year : '3 Year Term',
+    price : '$4.79 Per dekatherm',
+    fee : 4.79,
+    months : 36,
+  },
+  "addresses": {
+    "address1": "3030 Deltron ave",
+    "address2": "",
+    "city": "Denver",
+    "state": "CO",
+    "zip": 83030
+  },
+  "signature": {
+    "date": "2017-03-23T05:00:00.000Z",
+    "signature": "Marvin F Fritz",
+    "personAbove": "I certify I am the person above",
+    "agreeToSignElectronically": "I understand that I am using electronic means to sign this agreement",
+    "ipAddress": "97.121.82.81"
+  }
+}
